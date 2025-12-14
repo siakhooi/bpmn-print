1103a93211e6aa8c6b28677b495096505e611621
Siak Hooi <siakhooi@gmail.com>
2025-12-14 05:06:13 +0000

    fix set-version.sh bug

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -3,6 +3,6 @@
 # shellcheck disable=SC1091
 source ./release.env
 
-sed -i 'pyproject.toml' -e 's@version = .*@version = "'"$RELEASE_VERSION"'"@g'
+sed -i 'pyproject.toml' -e 's@^version = .*@version = "'"$RELEASE_VERSION"'"@'
 
 echo "bpmn-print $RELEASE_VERSION" > tests/expected-output/cli-version.txt
